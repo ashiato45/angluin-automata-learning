1a3565006928fe28073b7506378ea052a2b9f494
ashiato45 <katiiijp@gmail.com>
2018-03-23 19:04:01 +0900

    added html output

--- angluin.py
+++ angluin.py
@@ -1,10 +1,11 @@
 class Learner:
-    def __init__(self, t, a="01"):
+    def __init__(self, t, a="ab"):
         self.teacher = t
         self.alphabets = a
         self.rows = [""]
         self.columns = [""]
         self.table = self.make_table(self.rows, self.columns)
+        self.status = "init"
     def get_row(self, t, r):
         return [t[(r, c)] for c in self.columns]
     def update(self):
@@ -12,9 +13,7 @@ class Learner:
         for r in self.rows:
             for a in self.alphabets:
                 rows_trans.append(r + a)
-        print("rows_trans:", rows_trans)
         table_trans = self.make_table(rows_trans, self.columns)
-        print("table_trans:", table_trans)
         # is closed?
         for rt in rows_trans:
             found = False
@@ -26,20 +25,21 @@ class Learner:
                 # not closed
                 self.rows.append(rt)
                 self.table = self.make_table(self.rows, self.columns)
+                self.status = "not closed"
                 return False
         # closed
-        print("closed")
         # is consistent?
         for i in range(len(self.rows)):
             for j in range(i+1, len(self.rows)):
-                print("hoge:", self.rows[i], self.rows[j])
                 if self.get_row(self.table, self.rows[i]) == self.get_row(self.table, self.rows[j]):
                     for a in sef.alphabets:
                         if get_row(table_trans, self.rows[i]+a) != get_row(table_trans, self.rows[j]+a):
                             #incosnsitent!
                             newcol = [c+a for c in self.columns]
                             self.columns += newcol
+                            self.status = "not consistent"
                             return False
+        self.status = "ok"
         return True
     def make_table(self, rows_, columns_):
         t = {}
@@ -47,19 +47,68 @@ class Learner:
             for col in columns_:
                 t[(row, col)] = self.teacher(row + col)
         return t
+    def make_automaton(self):
+        def f(
+    def print_table(self):
+        rows_trans = []
+        for r in self.rows:
+            for a in self.alphabets:
+                rows_trans.append(r + a)
+        table_trans = self.make_table(rows_trans, self.columns)
+        def f(x):
+            if x == "":
+                return "[]"
+            else:
+                return x
+
+        print("<table rules=groups>")
+        print("<colgroup/>")
+        print("<thead>")
+        print("<tr>")
+        print("<td>*</td>")
+        for c in self.columns:
+            print("<td>%s</td>" % f(c))
+        print("</tr>")
+        print("</thead>")
+        print("<tbody>")                
+        for r in self.rows:
+            print("<tr>")
+            print("<td>%s</td>" % f(r))
+            for c in self.columns:
+                print("<td>%s</td>" % self.table[(r, c)])
+            print("</tr>")
+        print("</tbody>")
+        print("<tbody>")                        
+        for r in rows_trans:
+            print("<tr>")
+            print("<td>%s</td>" % f(r))            
+            for c in self.columns:
+                print("<td>%s</td>" % table_trans[(r, c)])
+            print("</tr>")
+        print("</tbody>")                                    
+        print("</table>")
 
 
 def teacher_even(s):
-    return (s.count("0")%2 == 0 and s.count("1")%2 == 0)
-l = Learner(teacher_even)
+    return (s.count("a")%2 == 0 and s.count("b")%2 == 0)
 
-print(l.rows, l.columns)
-print("table:", l.table)
-print("-"*80)
+
+                
+print("<html>")
+l = Learner(teacher_even)
+l.print_table()
+print("<hr/>")
+
+
+    
 while not l.update():
-    print(l.rows, l.columns)
-    print("table:", l.table)
-    print("-"*80)
+    l.print_table()
+    print("<hr/>")
+                
+
+
+print("</html>")
+    
 
         
         
